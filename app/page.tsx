f308e99b9de40ba507ac92d40d6aa09b3e07e01e
Zhi.Huang <zh.impact@outlook.com>
2023-07-14 01:41:52 +0800

    feat: main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+
+import { useUser } from '@auth0/nextjs-auth0/client'
+
 export default function Home() {
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <main>
-      <div>Home</div>
+      <h1 className="text-3xl text-center mt-8">Stripe demo</h1>
+      <div className="m-auto max-w-screen-xl">
+        {user ? (
+          <>
+            Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+          </>
+        ) : (
+          <a href="/api/auth/login">Login</a>
+        )}
+      </div>
     </main>
   )
 }
